4f8c57af22a367d868a9040f441c6bed86a8463d
Daniel Gutierrez <dgutierrezd@gmail.com>
2025-12-29 20:07:13 -0500

    Initial Commit

---
+++ Counsel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counsel
+//
+//  Created by Daniel Gutierrez on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counsel/CounselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounselApp.swift
+//  Counsel
+//
+//  Created by Daniel Gutierrez on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CounselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
